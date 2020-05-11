9fcd3ac42d8da158436ec13f6ccea1482081e647
gopinath_mb <gopinathjnv@gmail.com>
2020-05-11 18:06:16 +0530

    Initial MicroServices Project

---
+++ restful-web-services/src/main/java/com/gopi/rest/webservices/restfulwebservices/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.gopi.rest.webservices.restfulwebservices;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(RestfulWebServicesApplication.class);
+	}
+
+}
