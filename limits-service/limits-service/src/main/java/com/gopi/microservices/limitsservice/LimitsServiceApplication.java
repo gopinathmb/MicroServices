aa485fc5a5b6dd2c315cec10aa608ac1f422bb33
gopinath_mb <gopinathjnv@gmail.com>
2020-05-15 09:13:20 +0530

    limit service microservice

---
+++ limits-service/limits-service/src/main/java/com/gopi/microservices/limitsservice/LimitsServiceApplication.java
@@ -0,0 +1,14 @@
+package com.gopi.microservices.limitsservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LimitsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LimitsServiceApplication.class, args);
+	}
+
+}
+   
---
+++ limits-service/limits-service/src/main/java/com/gopi/microservices/limitsservice/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.gopi.microservices.limitsservice;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LimitsServiceApplication.class);
+	}
+
+}
