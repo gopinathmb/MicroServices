75656d2a4ba30b4867062c0a4e0b7742d256ca82
gopinath_mb <gopinathjnv@gmail.com>
2020-05-19 22:50:07 +0530

    Adding ZUUL Microservice

---
+++ netflix-zuul-api-gateway-server/src/main/java/com/gopi/microservices/netflixzuulapigatewayserver/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.gopi.microservices.netflixzuulapigatewayserver;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(NetflixZuulApiGatewayServerApplication.class);
+	}
+
+}
