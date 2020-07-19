b52425da082a5497db3a67cbf6509d1f6359c790
gopinath_mb <gopinathjnv@gmail.com>
2020-07-20 01:32:25 +0530

    Adding Spring cloud for Limit service

--- limits-service/src/main/java/com/gopi/microservices/limitsservice/LimitsConfigurationController.java
+++ limits-service/src/main/java/com/gopi/microservices/limitsservice/LimitsConfigurationController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.gopi.microservices.limitsservice.bean.LimitConfiguration;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 /**
  * @author gopinath_mb
@@ -18,6 +19,7 @@ public class LimitsConfigurationController
   @Autowired
   private Configuration configuration;
 
+  @HystrixCommand
   @GetMapping("/limits")
 public LimitConfiguration retrieveConfiguration() {
   
